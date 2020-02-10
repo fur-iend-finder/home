569f328eb1d21a5cbf4beaa938f7f33baf62d363
Hannah Yudkin <hannahyudkin@Hannahs-MacBook-Pro-2.local>
2020-02-10 18:32:20 -0500

    Started breed functionality

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// ---------------------- DOCUMENT READY ----------------------
+$(document).ready(function() {
+  $("#form-wrapper").hide();
+});
+
+// ---------------------- START SURVEY ON CLICK ----------------------
+$("#surveyBtn").on("click", function() {
+  $("#main-logo-wrapper").hide();
+  $("#form-wrapper").show();
+});
+
+// ---------------------- SUBMIT SURVEY ON CLICK ----------------------
+$("#submit").on("click", function() {
+  $("#survey").hide();
+  handlePetData();
+});
+
+// ---------------------- API CALL ----------------------
 petFinderAPI = "6gXqqaCV4rGHQFlZapWU444NSW4gmFlZDUnK9TYKUoBf0r2WPg";
 petFinderSecret = "Js4RQQVKwJyrmKglhelMQX9yiNDSqZ6b4c1p8hMS";
 petFinderToken =
@@ -10,80 +28,115 @@ petFinderToken =
 
 var dataString = `grant_type=client_credentials&client_id=${petFinderAPI}&client_secret=${petFinderSecret}`;
 
-function getToken() {
-  return $.ajax({
-    url: "https://api.petfinder.com/v2/oauth2/token",
-    method: "POST",
-    data: dataString
-  })
-}
-
-//questions = true or false
-//true add to the query string
-
-
-function buildQueryString() {
-  queryString = ""
-  if (confirm("dog")) {
-      queryString = queryString.concat("&type=dog")
-  }
-  if (confirm("cat")) {
-      queryString = queryString.concat("&type=cat")
-  }
-  if (confirm("small")) {
-      queryString = queryString.concat("&size=small,medium")
-  }
-  if (confirm("large")) {
-      queryString = queryString.concat("&size=large,xlarge")
-  }
-  if (confirm("young")) {
-      queryString = queryString.concat("&age=baby,young")
-  }
-  if (confirm("old")) {
-      queryString = queryString.concat("&age=adult,senior")
-  }
-  if (confirm("kids")) {
-      queryString = queryString.concat("&good_with_children=true")
-  }
-  if (confirm("cats")) {
-      queryString = queryString.concat("&good_with_dogs=true")
-  }
-  if (confirm("dogs")) {
-      queryString = queryString.concat("&good_with_cats=true")
-  }
-  //console.log(queryString)
-  return queryString;
-}
-
-
-
-  //console.log(petResponse);
-  // image1 = petResponse.animals[0].photos[0].small;
-  // console.log(image1)
-
-
 function petRequest(token) {
-  let query = buildQueryString()
-  return getToken().then (function(response) {
+  let query = buildQueryString();
+  return getToken().then(function(response) {
     return $.ajax({
       headers: {
         Authorization: `Bearer ${response.access_token}`
       },
       method: "GET",
       url: `https://api.petfinder.com/v2/animals/?${query}`
-    })
-  })
-
+    });
+  });
 }
 
 function handlePetData() {
   petRequest().then(function(response) {
-    console.log(response)
-    photo1= response.animals[0].photos[0].small
-    photo2= response.animals[0].photos[0].large
-    console.log(photo2)
-  })
+    console.log(response);
+    // photo1 = response.animals[0].photos[0].small;
+    // photo2 = response.animals[0].photos[0].large;
+    console.log(photo2);
+    breed = resonse.animals[0].breeds.primary;
+    console.log(breed)
+    return breed
+  });
+}
+
+function getToken() {
+  return $.ajax({
+    url: "https://api.petfinder.com/v2/oauth2/token",
+    method: "POST",
+    data: dataString
+  });
+}
+
+let queryString = "";
+let grabOptions = $(".option");
+
+// ---------------------- BUILD QUERY STRING WITH SURVEY ----------------------
+function buildQueryString() {
+  grabOptions.on("click", el => {
+    let grabValue = el.target.value;
+    if (el.currentTarget.checked == true && el.currentTarget.name == "type") {
+      queryString = queryString.concat(`&type=${grabValue}`);
+    } else if (
+      el.currentTarget.checked == true &&
+      el.currentTarget.name == "size"
+    ) {
+      queryString = queryString.concat(`&size=${grabValue}`);
+    } else if (
+      el.currentTarget.checked == true &&
+      el.currentTarget.name == "age"
+    ) {
+      queryString = queryString.concat(`&age=${grabValue}`);
+    } else if (
+      el.currentTarget.checked == true &&
+      el.currentTarget.name == "children"
+    ) {
+      queryString = queryString.concat(`&${grabValue}=${true}`);
+    }
+    if (
+      el.currentTarget.checked == true &&
+      el.currentTarget.name == "good_with_dogs"
+    ) {
+      queryString = queryString.concat(`&${grabValue}=${true}`);
+    }
+    if (
+      el.currentTarget.checked == true &&
+      el.currentTarget.name == "good_with_cats"
+    ) {
+      queryString = queryString.concat(`&${grabValue}=${true}`);
+    }
+  });
+  console.log(queryString);
+
+  return queryString;
+}
+
+buildQueryString();
+
+function getBreed() {
 
 }
 
-handlePetData()
+
+//retreives wiki article
+
+function getWikiArticle() {
+  var url = "https://en.wikipedia.org/w/api.php"; 
+
+  var params = {
+      action: "opensearch",
+      search: "siamese cat",
+      limit: "5",
+      namespace: "0",
+      format: "json"
+  };
+  
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+  
+  fetch(url)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(response) {
+        console.log(response);
+        webAddress = response[3][0];
+        console.log(webAddress)
+      })
+      .catch(function(error){console.log(error);});
+}
+
+getWikiArticle()
